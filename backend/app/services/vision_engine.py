56adb8868ef66caf2655729bcea202efa99ac7b4
Sunil Kumar R M <ph1230896@iitd.ac.in>
2026-06-03 01:18:23 +0530

    Update vision_engine.py

--- backend/app/services/vision_engine.py
+++ backend/app/services/vision_engine.py
@@ -6,30 +6,39 @@ from app.config import settings
 
 class VisionEngine:
     def __init__(self):
-        self.classifier = None
+        self.xray_classifier = None
+        self.ecg_classifier = None
         self.device = 0 if torch.cuda.is_available() else -1
 
-    def load_model(self):
-        """Load the vision model into system memory on startup."""
-        if self.classifier is None:
-            self.classifier = pipeline(
-                "image-classification",
-                model=settings.VISION_MODEL_NAME,
-                device=self.device
-            )
+    def load_models(self):
+        # print("Loading X-Ray Engine...")
+        self.xray_classifier = pipeline("image-classification", model=settings.XRAY_MODEL_NAME, device=self.device)
+        # print("Loading ECG Engine...")
+        self.ecg_classifier = pipeline("image-classification", model=settings.ECG_MODEL_NAME, device=self.device)
 
-    def analyze_image(self, image_bytes: bytes) -> tuple[str, float]:
-        """Process raw image byte-stream and execute inference."""
-        if self.classifier is None:
-            raise RuntimeError("Vision model components are not loaded. Call load_model() first.")
+    def analyze_image(self, image_bytes: bytes, scan_type: str) -> tuple[str, float]:
+        """Routes the image to the correct AI model based on scan_type."""
+        if self.xray_classifier is None or self.ecg_classifier is None:
+            raise RuntimeError("Vision models are not loaded. Call load_models() first.")
         
         image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-        raw_results = self.classifier(image)
         
-        if not raw_results:
-            return "Inconclusive / No features identified", 0.0
+        # Route to the correct mathematical model depends whteher ift he uploaded is xray or ecg
+        if scan_type == "xray":
+            raw_results = self.xray_classifier(image, top_k=None)
+        elif scan_type == "ecg":
+            raw_results = self.ecg_classifier(image, top_k=None)
+        else:
+            raise ValueError(f"Unknown scan type requested: {scan_type}")
             
         top_prediction = raw_results[0]
-        return top_prediction['label'], float(top_prediction['score'])
+        label = top_prediction['label']
+        confidence = float(top_prediction['score'])
+
+        # Confidence Threshold, to reject less sure predictoins
+        if confidence < 0.85:
+            return f"Unclear {scan_type.upper()} Scan - Please upload a clearer image.", confidence
+            
+        return label, confidence
 
 vision_engine = VisionEngine()
